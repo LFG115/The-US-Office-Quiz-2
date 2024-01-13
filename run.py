b5ff2afd966f5b05dd37bedbbca3b54d6e2c3312
LFG115 <>
2024-01-13 15:38:25 +0000

    add title, rules and brief description

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def begin():
+    """
+    The begining of the game starts with a breife summary of why amd what
+    its about then tells you the rules, and ask's your name.
+    """
+    print(" *WELCOME TO THE US OFFICE QUIZ!* \n\n\n")
+
+    print("This show has brought plenty of laughter and joy to our screens.")
+    print("It's easy to say the show has been nothing but spectacular!\n")
+    print("As you can tell I am a fan, I hope you enjoy.\n")
+
+    print("Rules: 15 Questions, 18 Points are up for grabs.\n")
+    print("Try not to cheat and good luck!\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+begin()
