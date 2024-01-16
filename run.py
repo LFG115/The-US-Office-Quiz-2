b8d859576f7ff822d74284e3bc89b820ebbf54f0
LFG115 <>
2024-01-16 15:00:08 +0000

    add correct/wrong answer solutions for q4 - q7

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ def question_one():
     print("\n")
     score = 0
 
-    #Question 1
+    # Question 1
     q1 = input("Q1: What item did Jim put in jelly ? \
     \n\n1. Mug \n\n2. Stapler \n\n3. Pen \n\n4. Calculator \n\nAnswer:  ")
     if q1 in ['1', '2', '3', '4']:
@@ -38,7 +38,7 @@ def question_one():
         else:
             wrong_answer("Stapler", score)
     else:
-        print("Please enter '1', '2', '3', '4'")
+        print("Incorrect entry, Please enter '1', '2', '3', '4'")
         question_one()
     question_two(score)
 
@@ -49,7 +49,7 @@ def question_two(score):
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 2
+    # Question 2
     q2 = input("Q2: Bob Vance has a business, what is it called ? \
     \n\n1. Vance clean & simple \n\n2. Vance reapir & recovery \n\n3. Vance refrigeration's \n\n4. Vance transportation's \n\nAnswer:  ")
     if q2 in ['1', '2', '3', '4']:
@@ -58,8 +58,8 @@ def question_two(score):
         else:
             wrong_answer("Vance refrigeration's", score)
     else:
-        print("Please enter '1', '2', '3', '4'")
-        question_two()
+        print("Incorrect entry, Please enter '1', '2', '3', '4'")
+        question_two(score)
     question_three(score)
 
 
@@ -69,7 +69,7 @@ def question_three(score):
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 3
+    # Question 3
     q3 = input("Q3: What department is Kevin located in ? \
     \n\n1. Sales \n\n2. Accounting \n\n3. Customer Services \n\n4. Quality assurance \n\nAnswer:  ")
     if q3 in ['1', '2', '3', '4']:
@@ -78,10 +78,9 @@ def question_three(score):
         else:
             wrong_answer("Accounting", score)
     else:
-        print("Please enter '1', '2', '3', '4'")
-        question_two()
-    question_four()(score)
-
+        print("Incorrect entry, Please enter '1', '2', '3', '4'")
+        question_three(score)
+    question_four(score)
 
 
 def question_four(score):
@@ -90,117 +89,157 @@ def question_four(score):
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 4
-    q4 = input("Q4: Who is the assistant to the reginal Manager ?")
+    # Question 4
+    q4 = input("Q4: Who is the assistant to the reginal Manager ? \
+    \n\n1. Jim \n\n2. Kevin \n\n3. Oscar \n\n4. Dwight \n\nAnswer:  ")
+    if q4 in ['1', '2', '3', '4']:
+        if int(q4) == 4:
+            score = correct_answer(score)
+        else:
+            wrong_answer("Dwight", score)
+    else:
+        print("Incorrect entry, Please enter '1', '2', '3', '4'")
+        question_four(score)
+    question_five(score)
 
 
-def question_five():
+def question_five(score):
     """
     Displays the fith question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 5
-    q5 = input("Q5: What is Jim's nickname according to Andy ?")
+    # Question 5
+    q5 = input("Q5: What is Jim's nickname according to Andy ? \
+    \n\n1. Tuna \n\n2. Ham \n\n3. Pikle \n\n4. Cream Cheese \n\nAnswer:  ")
+    if q5 in ['1', '2', '3', '4']:
+        if int(q5) == 1:
+            score = correct_answer(score)
+        else:
+            wrong_answer("Tuna", score)
+    else:
+        print("Incorrect entry, Please enter '1', '2', '3', '4'")
+        question_five(score)
+    question_six(score)
 
 
-def question_six():
+def question_six(score):
     """
     Displays the sixth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 6
-    q6 = input("Q6: Where is Dunder Mifflin located ?")
+    # Question 6
+    q6 = input("Q6: Where is Dunder Mifflin located ? \
+    \n\n1. Philadelphia \n\n2. Scranton \n\n3. New York \n\n4. Albany \n\nAnswer:  ")
+    if q6 in ['1', '2', '3', '4']:
+        if int(q6) == 2:
+            score = correct_answer(score)
+        else:
+            wrong_answer("Tuna", score)
+    else:
+        print("Incorrect entry, Please enter '1', '2', '3', '4'")
+        question_six(score)
+    question_seven(score)
 
 
-def question_seven():
+def question_seven(score):
     """
     Displays the seventh question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 7
-    q7 = input("Q7: What is the receptionist name ?")
+    # Question 7
+    q7 = input("Q7: What is the receptionist name ? \
+    \n\n1. Kelly \n\n2. Meredith \n\n3. Angela \n\n4. Pam \n\nAnswer:  ")
+    if q7 in ['1', '2', '3', '4']:
+        if int(q7) == 4:
+            score = correct_answer(score)
+        else:
+            wrong_answer("Pam", score)
+    else:
+        print("Incorrect entry, Please enter '1', '2', '3', '4'")
+        question_seven(score)
+    question_eight(score)
 
 
-def question_eight():
+def question_eight(score):
     """
     Displays the eighth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 8
+    # Question 8
     q8 = input("Q8: Who works in HR ?")
 
 
-def question_nine():
+def question_nine(score):
     """
     Displays the ninth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 9
+    # Question 9
     q9 = input("Q9: What has Michael Scott said before ?")
 
 
-def question_ten():
+def question_ten(score):
     """
     Displays the tenth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 10
+    # Question 10
     q10 = input("Q10: Who wins the "'Rear of the Year'" award ? (according to Michael)")
 
 
-def question_eleven():
+def question_eleven(score):
     """
     Displays the eleventh question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 11
+    # Question 11
     q11 = input("Q11: Who is Jan’s assistant ?")
 
 
-def question_tweleve():
+def question_tweleve(score):
     """
     Displays the twelveth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 12
+    # Question 12
     q12 = input("Q12: In the "'Booze Cruise'" episode who breaks up on the boat ?")
 
 
-def question_thirteen():
+def question_thirteen(score):
     """
     Displays the thirteenth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 13
+    # Question 13
     q13 = input("Q13: What's Pam's favourite flavour of yogurt ?")
 
 
-def question_fourteen():
+def question_fourteen(score):
     """
     Displays the fourteenth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 14
+    # Question 14
     q14 = input("Q14: Who started the fire ?")
 
 
-def question_fifteen():
+def question_fifteen(score):
     """
     Displays the fifteenth question.
     Player must answer the questions to continue
     and then gives a score.
     """
-    #Question 15
+    # Question 15
     q15 = input("Q15: Who is the final couple we see getting married ?")
 
 
@@ -220,15 +259,17 @@ def correct_answer(score):
     1 to the score.
     """
     score += 1
-    print("Correct, Well Done!")
+    print("Correct! Well Done!")
     print("Score : ", score)
     print("\n")
     return score
+
 
 def clear():
     """
     Function to clear the terminal when called.
     """
     os.system("clear")
+
 
 begin()
