53a2c50c788dddaf079ae2f2612cb51fc016f02f
LFG115 <>
2024-01-16 19:04:31 +0000

    fix spellings and empty spaces

--- run.py
+++ run.py
@@ -2,6 +2,35 @@
 import os
 
 
+def clear():
+    """
+    Function to clear the terminal when called.
+    """
+    os.system("clear")
+
+
+def correct_answer(score):
+    """
+    Displays the correct answer message and adds
+    1 to the score.
+    """
+    score += 1
+    print("Correct! Well Done!")
+    print("Score : ", score)
+    print("\n")
+    return score
+
+
+def wrong_answer(correct_answer, score):
+    """
+    Displays the incorrect answer message along with correct answer,
+    while displaying previous score result.
+    """
+    print("Wrong Unlucky! The answer is " + correct_answer)
+    print("Score: ", score)
+    print("\n")
+
+
 def home():
     """
     The begining of the game starts with a breife summary of why amd what
@@ -15,7 +44,7 @@ def home():
     print("As you will tell I am a fan, I do hope you enjoy.\n")
 
     print("Rules: 15 Questions, 15 Points are up for grabs.")
-    print("Please answer using keys '1', '2', '3', '4' and some questions '5'\n")
+    print("Please answer using keys '1', '2', '3', '4' and 1 question '5'\n")
     print("Try not to cheat and good luck!\n")
 
     player_name = input("Enter your name: ")
@@ -76,7 +105,8 @@ def question_two(score):
     """
     # Question 2
     q2 = input("Q2: Bob Vance has a business, what is it called ? \
-    \n\n1. Vance clean & simple \n\n2. Vance reapir & recovery \n\n3. Vance refrigeration's \n\n4. Vance transportation's \n\nAnswer:  ")
+    \n\n1. Vance clean & simple \n\n2. Vance reapir & recovery \
+    \n\n3. Vance refrigeration's \n\n4. Vance transportation's \n\nAnswer:  ")
     if q2 in ['1', '2', '3', '4']:
         if int(q2) == 3:
             score = correct_answer(score)
@@ -97,7 +127,8 @@ def question_three(score):
     """
     # Question 3
     q3 = input("Q3: What department is Kevin located in ? \
-    \n\n1. Sales \n\n2. Accounting \n\n3. Customer Services \n\n4. Quality assurance \n\nAnswer:  ")
+    \n\n1. Sales \n\n2. Accounting \n\n3. Customer Services \
+    \n\n4. Quality assurance \n\nAnswer:  ")
     if q3 in ['1', '2', '3', '4']:
         if int(q3) == 2:
             score = correct_answer(score)
@@ -160,7 +191,8 @@ def question_six(score):
     """
     # Question 6
     q6 = input("Q6: Where is Dunder Mifflin located ? \
-    \n\n1. Philadelphia \n\n2. Scranton \n\n3. New York \n\n4. Albany \n\nAnswer:  ")
+    \n\n1. Philadelphia \n\n2. Scranton \
+    \n\n3. New York \n\n4. Albany \n\nAnswer:  ")
     if q6 in ['1', '2', '3', '4']:
         if int(q6) == 2:
             score = correct_answer(score)
@@ -223,7 +255,8 @@ def question_nine(score):
     """
     # Question 9
     q9 = input("Q9: What has Michael Scott said before ? \
-    \n\n1. 'That's what she said' \n\n2. 'Why always me' \n\n3. 'Beets are life' \n\n4. 'Did I stutter' \n\nAnswer:  ")
+    \n\n1. 'That's what she said' \n\n2. 'Why always me' \
+    \n\n3. 'Beets are life' \n\n4. 'Did I stutter' \n\nAnswer:  ")
     if q9 in ['1', '2', '3', '4']:
         if int(q9) == 1:
             score = correct_answer(score)
@@ -244,7 +277,8 @@ def question_ten(score):
     """
     # Question 10
     q10 = input("Q10: Who wins the "'Rear of the Year'" award ? (according to Michael) \
-    \n\n1. Pam  \n\n2. Angela \n\n3. Ryan \n\n4. Dwight \n\n5. Kelly \n\nAnswer:  ")
+    \n\n1. Pam  \n\n2. Angela \n\n3. Ryan \
+    \n\n4. Dwight \n\n5. Kelly \n\nAnswer:  ")
     if q10 in ['1', '2', '3', '4', '5']:
         if int(q10) == 3:
             score = correct_answer(score)
@@ -307,7 +341,8 @@ def question_thirteen(score):
     """
     # Question 13
     q13 = input("Q13: What's Pam's favourite flavour of yogurt ? \
-    \n\n1. Vanilla \n\n2. Peach \n\n3. Strawberry & kiwi \n\n4. Mixed berry \n\nAnswer:  ")
+    \n\n1. Vanilla \n\n2. Peach \n\n3. Strawberry & kiwi \
+    \n\n4. Mixed berry \n\nAnswer:  ")
     if q13 in ['1', '2', '3', '4']:
         if int(q13) == 4:
             score = correct_answer(score)
@@ -328,7 +363,8 @@ def question_fourteen(score):
     """
     # Question 14
     q14 = input("Q14: Who started the fire ? \
-    \n\n1. Jim started the fire! \n\n2. Ryan started the fire! \n\n3. Kevin started the fire! \n\n4. Oscar started the fire! \n\nAnswer:  ")
+    \n\n1. Jim started the fire! \n\n2. Ryan started the fire! \
+    \n\n3. Kevin started the fire! \n\n4. Oscar started the fire! \n\nAnswer: ")
     if q14 in ['1', '2', '3', '4']:
         if int(q14) == 2:
             score = correct_answer(score)
@@ -349,7 +385,8 @@ def question_fifteen(score):
     """
     # Question 15
     q15 = input("Q15: Who is the final couple we see getting married ? \
-    \n\n1. Jim and Pam \n\n2. Dwight and Angela \n\n3. Kelly and Ryan \n\n4. Michael and Holly \n\nAnswer:  ")
+    \n\n1. Jim and Pam \n\n2. Dwight and Angela \n\n3. Kelly and Ryan \
+    \n\n4. Michael and Holly \n\nAnswer:  ")
     if q15 in ['1', '2', '3', '4']:
         if int(q15) == 2:
             score = correct_answer(score)
@@ -359,35 +396,6 @@ def question_fifteen(score):
         print("Incorrect entry, Please enter '1', '2', '3', '4'")
         print("\n")
         question_fifteen(score)
-    
-
-def wrong_answer(correct_answer, score):
-    """
-    Displays the incorrect answer message along with correct answer,
-    while displaying previous score result.
-    """
-    print("Wrong Unlucky! The answer is " + correct_answer)
-    print("Score: ", score)
-    print("\n")
-
-
-def correct_answer(score):
-    """
-    Displays the correct answer message and adds
-    1 to the score.
-    """
-    score += 1
-    print("Correct! Well Done!")
-    print("Score : ", score)
-    print("\n")
-    return score
-
-
-def clear():
-    """
-    Function to clear the terminal when called.
-    """
-    os.system("clear")
 
 
 home()
